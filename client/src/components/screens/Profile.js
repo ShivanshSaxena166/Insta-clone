aa83b4effcb7487e7366041dfc5e7eca9bc19b3a
unknown <shivanshkumar166@gmail.com>
2020-08-31 00:15:25 +0530

    profile

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,9 +1,21 @@
 import React from 'react';
 const Profile =()=>{
      return(
-         <h1>
-             Profile
-         </h1>
+        <div>
+            <div>
+<div>
+    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+    src="https://images.unsplash.com/photo-1499281851221-5bce936073b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1059&q=80"
+    />
+</div>
+<div>
+    <h4>
+        Alan walker
+    </h4>
+</div>
+
+            </div>
+        </div>
      )
 }
 export default Profile
