1d52cf78a3ea6ec7582061f9a4a2fc26f68ff37d
unknown <shivanshkumar166@gmail.com>
2020-08-31 01:28:57 +0530

    profile

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,17 +2,39 @@ import React from 'react';
 const Profile =()=>{
      return(
         <div>
-            <div>
+            <div style ={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
 <div>
     <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
-    src="https://images.unsplash.com/photo-1499281851221-5bce936073b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1059&q=80"
+    src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
     />
 </div>
 <div>
     <h4>
-        Alan walker
+      Kysten benendtt
     </h4>
+    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+        <h6> 6.7k post</h6>
+        <h6> 2.3m followers</h6>
+        <h6>568 following</h6>
+    </div>
+
 </div>
+
+            </div>
+            <div className="gallery">
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+<img src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
 
             </div>
         </div>
