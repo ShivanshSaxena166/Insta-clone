6ac3c2ced535ddbed9cfd7dcb800871fc69cbe98
unknown <shivanshkumar166@gmail.com>
2020-08-31 18:52:00 +0530

    home  ui

--- client/src/components/screens/home.js
+++ client/src/components/screens/home.js
@@ -1,9 +1,82 @@
 import React from 'react';
 const HOME =()=>{
      return(
-         <h1>
-             home
-         </h1>
+         <div className="home">
+       <div className="card home-card">
+<h5>Krysten Benedett</h5>
+<div className="card-image">
+    <img src="https://images.unsplash.com/photo-1512850183-6d7990f42385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" />
+</div>
+<div className="card-content">
+
+    <h6>
+        title
+    </h6>
+    <h6>
+        stay beautiful
+    </h6>
+    <input type="text" placeholder="add a comment" />
+</div>
+
+       </div>
+
+       <div className="card home-card">
+<h5>Krysten Benedett</h5>
+<div className="card-image">
+    <img src="https://images.unsplash.com/photo-1512850183-6d7990f42385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" />
+</div>
+<div className="card-content">
+
+    <h6>
+        title
+    </h6>
+    <h6>
+        stay beautiful
+    </h6>
+    <input type="text" placeholder="add a comment" />
+</div>
+
+       </div>
+
+
+
+
+       <div className="card home-card">
+<h5>Krysten Benedett</h5>
+<div className="card-image">
+    <img src="https://images.unsplash.com/photo-1512850183-6d7990f42385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" />
+</div>
+<div className="card-content">
+
+    <h6>
+        title
+    </h6>
+    <h6>
+        stay beautiful
+    </h6>
+    <input type="text" placeholder="add a comment" />
+</div>
+
+       </div>
+       <div className="card home-card">
+<h5>Krysten Benedett</h5>
+<div className="card-image">
+    <img src="https://images.unsplash.com/photo-1512850183-6d7990f42385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" />
+</div>
+<div className="card-content">
+<i class="material-icons">favorite</i>
+
+    <h6>
+        title
+    </h6>
+    <h6>
+        stay beautiful
+    </h6>
+    <input type="text" placeholder="add a comment" />
+</div>
+
+       </div>
+         </div>
      )
 }
 export default HOME
