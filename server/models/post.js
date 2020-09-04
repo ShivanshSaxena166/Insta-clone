934c097f2439f516f30037b02e9383a3726b02a1
unknown <shivanshkumar166@gmail.com>
2020-09-04 23:26:23 +0530

    backend for like and unlike

--- server/models/post.js
+++ server/models/post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { route } = require('../routes/auth')
 const {ObjectId} = mongoose.Schema.Types
 const postSchema = new mongoose.Schema({
     title:{
@@ -13,10 +14,44 @@ const postSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    likes:[{type:ObjectId,ref:"User"}],
     postedBy:{
 type:ObjectId,
 ref:"User"
     }
 })  
+
+route.put('/like',requiredLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err)
+        {
+            return res.status(422).json({error:err})
+        }
+        else{
+            res.json(result)
+        }
+    })
+
+})
+route.put('/unlike',requiredLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err)
+        {
+            return res.status(422).json({error:err})
+        }
+        else{
+            res.json(result)
+        }
+    })
+
+})
 const Post =mongoose.model("Post",postSchema)
 module.exports =Post
