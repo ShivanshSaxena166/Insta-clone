846b34116f2c112eb14fbc8d4a719dd7331fdc16
unknown <shivanshkumar166@gmail.com>
2020-09-05 20:57:25 +0530

    like feature

--- client/src/components/screens/home.js
+++ client/src/components/screens/home.js
@@ -1,6 +1,10 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useContext} from 'react';
+import {UserContext}  from '../../App'
 const HOME =()=>{
+   
     const [data,setdata] = useState([])
+    const {state,dispatch}=useContext(UserContext)
+   
     useEffect(()=>{
 
 fetch('/allpost',{
@@ -16,6 +20,66 @@ fetch('/allpost',{
 })
 
     },[])
+
+    const likePost =(id)=>{
+
+       fetch('/like',{
+           method:"put",
+           headers:{
+               "Content-Type":"application/json",
+               "Authorization":"Bearer "+localStorage.getItem("jwt")},
+                body:JSON.stringify({
+                    postId:id
+                })
+           
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           const newData = data.map(item=>{
+               if(item._id==result._id)
+               {
+                   return result
+               }
+               else{
+return item
+               }
+
+           }
+           )
+           setdata(newData)
+       })
+
+    }
+    const unlikePost =(id)=>{
+
+        fetch('/unlike',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")},
+                 body:JSON.stringify({
+                     postId:id
+                 })
+            
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.map(item=>{
+                if(item._id==result._id)
+                {
+                    return result
+                }
+                else{
+ return item
+                }
+ 
+            }
+            )
+            setdata(newData)
+        })
+ 
+     }
+
      return(
          <div className="home">
 
@@ -29,10 +93,22 @@ fetch('/allpost',{
                 <img src= {item.photo} />
             </div>
             <div className="card-content">
+           
+            <i className="material-icons" style={{color:"red"}}>favorite</i>
+           
+{
+    item.likes.includes(state._id)?<i className="material-icons"  onClick= {()=>{unlikePost(item._id)}}>thumb_down</i>:<i className="material-icons" onClick= {()=>{likePost(item._id)}}>thumb_up</i>
+}
+
             
+            
+                <h6>
+                {item.likes.length} likes
+                </h6>
                 <h6>
                 {item.title}
                 </h6>
+
                 <h6>
                   {item.body}
                 </h6>
