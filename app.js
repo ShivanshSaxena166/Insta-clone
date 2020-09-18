7bd209318ff44fa0bfebabf15518c13370e1dc1e
unknown <shivanshkumar166@gmail.com>
2020-09-19 01:23:10 +0530

    production changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 const mongoose = require('mongoose')
-const {MONGOURI} = require('./keys.js')
+const {MONGOURI} = require('./config/keys.js')
 const auth=require('./routes/auth.js')
 const Post= require('./models/post')
 
@@ -21,9 +21,16 @@ app.use(express.json())
 app.use(auth)
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
+if(process.env.NODE_ENV=="production")
+{
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.thml'))
+    })
+}
 
-
-app.listen(PORT,()=>{
+app.listen(process.env.PORT||5000,()=>{
     console.log("server is running on ",PORT)
 })
 
